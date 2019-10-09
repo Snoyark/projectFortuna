2b91026c2b7ad0bd033e466d2ba059599f75d518
Neel Venugopal <neelv7@berkeley.edu>
2019-10-09 18:36:55 -0500

    Updates

---
+++ precache-manifest.a9f348eb8958918c47a225056b2a65ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2750b382f8a8d9fe3fc5a97b8ff253c",
+    "url": "/fortuna/index.html"
+  },
+  {
+    "revision": "051a10587c7ac3346672",
+    "url": "/fortuna/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "3e00618c555aae5cb20a",
+    "url": "/fortuna/static/js/2.eeb6d0ef.chunk.js"
+  },
+  {
+    "revision": "051a10587c7ac3346672",
+    "url": "/fortuna/static/js/main.1133a3c8.chunk.js"
+  },
+  {
+    "revision": "521dcac690b4fe6df690",
+    "url": "/fortuna/static/js/runtime-main.bd955684.js"
+  }
+]);
