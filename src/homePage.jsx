97a9799ebc652323fa7c8e36fe3c42e87f31fc39
Neel Venugopal <neelv7@berkeley.edu>
2019-10-10 19:39:50 -0500

    progress towards multipages

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,19 +1,42 @@
 import DiceRoll from './DiceRoll';
 import React from 'react';
+import {
+    Router,
+    Route,
+    Switch,
+    Link
+} from "react-router-dom";
+
+import styled from '@emotion/styled'
+
+const DiceContainer = styled('div');
 
 function HomePage() {
-
     return (
-        <div>
-            <h1>
-                Dice Roller:
-            </h1>
-            <h3>
-                Click on one of the following buttons: it will give you the value of throwing a dice once.
-            </h3>
-            <DiceRoll />
-        </div>
+        <Router>
+            <div>
+                <DiceContainer>
+                    <h1>
+                        Dice Roller:
+                    </h1>
+                    <h3>
+                        Click on one of the following buttons: it will give you the value of throwing a dice once.
+                    </h3>
+                    <DiceRoll />
+                    <Link to="/slogan">Link to Slogan</Link>
+                </DiceContainer>
+                <Switch>
+                    <Route path="/slogan">
+                        <Slogan />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
+}
+
+function Slogan() {
+    return <h2>May the gods be ever in your favor.</h2>
 }
 
 export default HomePage;
