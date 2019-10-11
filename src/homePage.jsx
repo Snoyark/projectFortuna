16485618cc2ee1cd0213fec60a1f022c27e11011
Neel Venugopal <neelv7@berkeley.edu>
2019-10-11 13:36:14 -0500

    added dummy link

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,7 +1,7 @@
 import DiceRoll from './DiceRoll';
 import React from 'react';
 import {
-    Router,
+    BrowserRouter as Router,
     Route,
     Switch,
     Link
@@ -9,25 +9,41 @@ import {
 
 import styled from '@emotion/styled'
 
-const DiceContainer = styled('div');
+const DiceContainer = styled('div')({});
+
+const DiceHomePage = () => (
+    <DiceContainer>
+        <h1>
+            Dice Roller:
+        </h1>
+        <h3>
+            Click on one of the following buttons: it will give you the value of throwing a dice once.
+        </h3>
+        <DiceRoll />
+    </DiceContainer>
+)
 
 function HomePage() {
     return (
         <Router>
             <div>
-                <DiceContainer>
-                    <h1>
-                        Dice Roller:
-                    </h1>
-                    <h3>
-                        Click on one of the following buttons: it will give you the value of throwing a dice once.
-                    </h3>
-                    <DiceRoll />
+                <div>
+                    <Link to="/">Home</Link>
+                    <hr />
                     <Link to="/slogan">Link to Slogan</Link>
-                </DiceContainer>
+                </div>
+                {/* <Route path="/slogan">
+                    <Slogan />
+                </Route>
+                <Route to="/">
+                    {DiceHomePage}
+                </Route> */}
                 <Switch>
                     <Route path="/slogan">
                         <Slogan />
+                    </Route>
+                    <Route path="/">
+                        <DiceHomePage />
                     </Route>
                 </Switch>
             </div>
